d002439de849b9ad71c610eddad6efd82338d8b8
Czespo <czespo.saj@gmail.com>
2024-04-29 15:17:49 +0200

    added date API

--- app/routes.php
+++ app/routes.php
@@ -44,6 +44,52 @@ return function (App $app)
         return $response;
     });
 
+    $app->get('/date/api/{timestamp:\d+}', function (Request $request, Response $response, array $args)
+    {
+        $timestamp = intval($args['timestamp']);
+        $body = [
+            'unix' => $timestamp,
+            //                                           Convert to seconds.
+            'utc' => date('D, d M Y H:i:s', intval($timestamp / 1000)) . ' GMT'
+        ];
+
+        $response = $response->withHeader("Content-Type", "application/json");
+        $response->getBody()->write(json_encode($body, JSON_UNESCAPED_SLASHES));
+        return $response;
+    });
+
+    // .* pattern to allow forward slashes.
+    $app->get('/date/api[/{date:.*}]', function (Request $request, Response $response, array $args)
+    {
+        $response = $response->withHeader("Content-Type", "application/json");
+        if (!empty($args['date']))
+        {
+            try
+            {
+                $date = new DateTime($args['date']);
+            }
+            catch(Exception $e)
+            {
+                $response = $response->withStatus(400);
+                $response->getBody()->write(json_encode(['error' => 'Invalid Date'], JSON_UNESCAPED_SLASHES));
+                return $response;
+            }
+        }
+        else
+        {
+            $date = new DateTime();
+        }
+
+        $body = [
+            //                      Convert to milliseconds.
+            'unix' => $date->format('U') * 1000,
+            'utc' => $date->format('D, d M Y H:i:s') . ' GMT'
+        ];
+
+        $response->getBody()->write(json_encode($body, JSON_UNESCAPED_SLASHES));
+        return $response;
+    });
+
     $app->post('/api/shorturl', function (Request $request, Response $response)
     {
         $body = $request->getParsedBody();
